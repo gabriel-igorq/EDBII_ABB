6c084e063492cf37495dd04f513d4047f69f20b4
Giordano Rodrigues <giordano2dcc@gmail.com>
2018-10-28 20:47:27 -0300

    add reading input files functionality, but not interpretating

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,37 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "../include/Node.hpp"
 #include "../include/BinarySearchTree.hpp"
 
 
-int main() {
-	std::cout << "Iniciando Testes:" << std::endl;
-
+int main(int argc, char *argv[]) {
 	BinarySearchTree tree;
 
-	//Testando construtor
-	Node* root = tree.getRoot();
-	
-	tree.insert(8);
-	tree.insert(4);
-	tree.insert(10);
-	tree.insert(12);
-	tree.insert(5);
-	tree.insert(14);
+	string tree_file = argv[1];
+	std::ifstream tree_inputs;
+	tree_inputs.open("data/" + tree_file);
+	if ( tree_inputs.is_open() ) {
+		string str;
+		while ( std::getline(tree_inputs, str, ' ') ) {
 
-	std::cout << tree.toString() << endl;
+			int key = std::stoi(str);
+			tree.insert(key);
 
-	//Testando busca
-	Node* tmp = tree.search(10);
-	if(tmp->key == 10) {
-		std::cout << "Busca funcionando!" << std::endl;    	
+		}
 	}
+	std::cout << tree.toString() << std::endl;
 
-	std::cout << "Cheia? " << tree.isFull() << endl;
-	std::cout << "Completa? " << tree.isComplete() << endl;	
-	
-	tree.remove(8);
-	std::cout << tree.toString() << endl;
-	
-	
-	// Testando remoção de elemento existente
-	/*if ( tree.remove(10) )
-		std::cout << "Remoção de elemento existente funcionando!" << std::endl;
-	else
-		std::cout << "Remoção de elemento existente falhou!" << std::endl;
+	string commands_file = argv[2];
+	std::ifstream command_inputs;
+	command_inputs.open("data/" + commands_file);
+	if ( command_inputs.is_open() ) {
+		string line;
+		while ( std::getline(command_inputs, line) ) {
 
-	
-	// Testando remoção de elemento não existente
-	if ( tree.remove(10) )
-		std::cout << "Remoção de elemento não existente funcionando!" << std::endl;
-	else
-		std::cout << "Remoção de elemento não existente falhou!" << std::endl;
-	*/
-/*
-	std::cout << tree.toString() << endl;
-	tree.remove(8);
-	std::cout << tree.toString() << endl;
+			std::cout << strtok(line,' ') << std::endl;
 
-	std::cout << "-----------ENÉSIMO-----------" << endl;
-	std::cout << tree.enesimo(1) << endl;
-	std::cout << tree.enesimo(2) << endl;
-	std::cout << tree.enesimo(3) << endl;
-	std::cout << tree.enesimo(4) << endl;
-	std::cout << tree.enesimo(5) << endl;
-	std::cout << tree.enesimo(6) << endl;
-
-	std::cout << "-----------POSITION-----------" << endl;
-	std::cout << tree.position(8) << endl;
-	std::cout << tree.position(4) << endl;
-	std::cout << tree.position(5) << endl;
-	std::cout << tree.position(10) << endl;
-	std::cout << tree.position(12) << endl;
-	std::cout << tree.position(15) << endl;
-
-	std::cout << "-----------MEDIAN-----------" << endl;
-	std::cout << tree.median() << endl;
-
-*/
+		}
+	}
 	return 0;
 }
